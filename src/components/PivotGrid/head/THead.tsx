753eca573dac16ae251c56e57a23fbf38c89b33d
C5m7b4 <c5m7b4@gmail.com>
2024-06-10 16:15:47 -0500

    adds THead

--- src/components/PivotGrid/head/THead.tsx
+++ src/components/PivotGrid/head/THead.tsx
@@ -1,83 +1,59 @@
 import { IRow } from "../dropTargets/Rows";
 import { IValue } from "../dropTargets/Values";
-import styled, { useTheme } from "styled-components";
+import styled from "styled-components";
 
-export const Th = styled.th<{
-  $width?: number;
-  $align?: number;
-}>`
-  display: grid;
-  grid-template-columns: 1fr 20px 2px;
+const TableHeader = styled.div`
+  display: flex;
+  border-bottom: 1px solid black;
   background-color: ${(props) => props.theme.colors.table.excel};
   color: ${(props) => props.theme.colors.table.thText};
-  font-weight: ${(props) => props.theme.fontWeights.bold};
-  padding: 0 0 0 5px;
-  width: ${(props) => props.$width}px;
-  max-width: ${(props) => props.$width}px;
-  overflow: clip;
-  text-overflow: ellipsis;
-  text-align: ${(props) =>
-    props.$align === 0 ? "left" : props.$align === 1 ? "center" : "right"};
+  padding: 3px 8px;
 `;
+
+const TableHeaderCell = styled.div<{ $width: number; $align: string }>`
+  width: ${(props) => props.$width}px;
+  text-align: ${(props) => props.$align};
+`;
+
 interface Props {
   rows: IRow[];
-  setShowContextMenu: (b: boolean) => void;
-  handleSortDirection: (e: React.MouseEvent<HTMLSpanElement>, r: IRow) => void;
   values: IValue[];
-  handleContextMenu: (e: React.MouseEvent<HTMLDivElement>, f: string) => void;
+  calculateWidth: () => number;
 }
 
-const Thead: React.FC<Props> = ({
-  rows = [],
-  setShowContextMenu,
-  handleSortDirection,
-  values,
-  handleContextMenu,
-}) => {
-  const theme = useTheme();
-  return (
-    <thead>
-      <tr style={{ display: "flex" }}>
-        <Th style={{ width: "25px" }}></Th>
-        {rows &&
-          rows.map((r, i) => {
-            if (i > 0) {
-              // only print out the first row becuase everything else will be nested
-              return;
-            }
-            return (
-              <Th
-                key={`th-${i}`}
-                onContextMenu={(e) => {
-                  e.preventDefault();
-                  setShowContextMenu(false);
-                }}
-              >
-                <span> {r.label}</span>
-                <span onClick={(e) => handleSortDirection(e, r)}>
-                  {r.direction == "asc" ? (
-                    <ion-icon name="arrow-round-down"></ion-icon>
-                  ) : (
-                    <ion-icon name="arrow-round-up"></ion-icon>
-                  )}
-                </span>
-              </Th>
-            );
-          })}
-        {values &&
-          values.map((v, idx) => (
-            <Th
-              key={`th-v-${idx}`}
-              onContextMenu={(e) => {
-                handleContextMenu(e, v.label);
-              }}
-            >
-              {v.alias ? v.alias : `${v.aggregator} of ${v.label}`}
-            </Th>
-          ))}
-      </tr>
-    </thead>
-  );
-};
+function THead(props: Props) {
+  const { rows, values, calculateWidth } = props;
 
-export default Thead;
+  return (
+    <div>
+      <TableHeader className="seudo-table-header">
+        {rows.map((r, i) => {
+          if (i === 0) {
+            return (
+              <TableHeaderCell
+                $align={"left"}
+                $width={calculateWidth()}
+                key={`th-${i}`}
+              >
+                {r.label}
+              </TableHeaderCell>
+            );
+          }
+        })}
+        {values.map((v, i) => {
+          return (
+            <TableHeaderCell
+              $align={"right"}
+              $width={calculateWidth() as number}
+              key={`th-v${i}`}
+            >
+              Sum of {v.label}
+            </TableHeaderCell>
+          );
+        })}
+      </TableHeader>
+    </div>
+  );
+}
+
+export default THead;
