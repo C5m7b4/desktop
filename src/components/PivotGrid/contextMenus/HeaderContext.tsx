06b658edc3d36563ced03e6769ec87698566909b
C5m7b4 <c5m7b4@gmail.com>
2024-06-10 21:43:58 -0500

    added formatters start

--- src/components/PivotGrid/contextMenus/HeaderContext.tsx
+++ src/components/PivotGrid/contextMenus/HeaderContext.tsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import Select, { SelectProps } from "../../Select";
+import { formatNumber, formatCurrency } from "../formatters";
+import { IValue } from "../dropTargets/Values";
+import NumberField from "../../NumberField";
+import Checkbox from "../../Checkbox";
 
 const Div = styled.div`
   border-bottom: 1px solid black;
   margin-bottom: 5px;
-  padding: 2px 15px;
+  padding: 5px 15px;
+  font-size: 1rem;
+  font-weight: bold;
   cursor: pointer;
+  z-index: 100;
   transition: all 0.4s ease;
   &:hover {
     background-color: ${(props) =>
@@ -14,24 +22,67 @@ const Div = styled.div`
   }
 `;
 
+const H3 = styled.div`
+  padding: 5px 15px;
+`;
+
 interface Props {
   top: number;
   left: number;
   column: string;
   handleAliasClick: (c: string) => void;
+  values: IValue[];
+  setValues: (i: IValue[]) => void;
 }
 
 function HeaderContext(props: Props) {
-  const { top, left, column, handleAliasClick } = props;
+  const { top, left, column, handleAliasClick, values, setValues } = props;
+  const [isPercentage, setIsPercentage] = useState(false);
 
-  const handleFormatterClick = (c: string) => {};
+  const handleFormatterClick = (c: SelectProps) => {
+    const selectedValue = values.filter((v) => v.label === column)[0];
+    const copy = [...values];
+    const index = values.findIndex((v) => v.label === column);
+    switch (c.value) {
+      case "formatNumber":
+        selectedValue.formatter = formatNumber;
+        break;
+      case "formatCurrency":
+        selectedValue.formatter = formatCurrency;
+        break;
+      case "remove":
+        selectedValue.formatter = undefined;
+        break;
+      default:
+        selectedValue.formatter = formatNumber;
+        break;
+    }
+    copy.splice(index, 1, selectedValue);
+    setValues(copy);
+  };
+
+  const formatters: SelectProps[] = [
+    {
+      label: "Format Number",
+      value: "formatNumber",
+    },
+    {
+      label: "Format Currency",
+      value: "formatNumber",
+    },
+    {
+      label: "Remove formatter",
+      value: "remove",
+    },
+  ];
 
   const style = {
     border: "1px solid black",
     position: "absolute",
     top: `${top}px`,
     left: `${left}px`,
-    backgroundColor: "rgba(255, 255, 255, 0.9)",
+    width: "200px",
+    backgroundColor: "rgba(255, 255, 255, 1)",
     boxShadow: "2px 4px 5px rgba(0, 0, 0, 0.3)",
   };
   return (
@@ -39,12 +90,30 @@ function HeaderContext(props: Props) {
       <Div className="header-element" onClick={() => handleAliasClick(column)}>
         Create Alias
       </Div>
-      <Div
-        className="header-element"
-        onClick={() => handleFormatterClick(column)}
+      <div>
+        <H3>Select a Formatter</H3>
+        <Select
+          onChange={handleFormatterClick}
+          placeholder="Formatters"
+          options={formatters}
+        />
+      </div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
       >
-        Add Formatter
-      </Div>
+        <H3>Decimal Places</H3>
+        <NumberField />
+      </div>
+      <Checkbox
+        label="Percentage"
+        checked={isPercentage}
+        onChange={setIsPercentage}
+      />
     </div>
   );
 }
