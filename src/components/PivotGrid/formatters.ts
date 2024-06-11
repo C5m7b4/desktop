9c1061941c73a55d2b68f93dfb7026503753cb36
C5m7b4 <c5m7b4@gmail.com>
2024-06-11 16:32:04 -0500

    adds format ability

--- src/components/PivotGrid/formatters.ts
+++ src/components/PivotGrid/formatters.ts
@@ -1,24 +1,24 @@
 import { pad } from "../../utils/pad";
 
+export type INumberFormatter = (i: string, n: number) => string;
+export type IFormatter = INumberFormatter | null;
+
 export function formatNumber(input: string, numberOfDecimals: number = 2) {
-  const periodPosition = input.indexOf(".");
-  const left = input.substring(0, periodPosition);
-  const right = input.substr(periodPosition);
-  return (
-    left +
-    "." +
-    Number(pad(right, numberOfDecimals, "0", "left")).toFixed(numberOfDecimals)
-  );
+  const periodPosition = input.toString().indexOf(".");
+  if (periodPosition < 0) {
+    return input + "." + pad("", numberOfDecimals, "0", "left");
+  }
+  const left = input.toString().substring(0, periodPosition);
+  const right = input.toString().substr(periodPosition + 1);
+  return left + "." + pad(right, numberOfDecimals, "0", "left");
 }
 
 export function formatCurrency(input: string, numberOfDecimals: number = 2) {
-  const periodPosition = input.indexOf(".");
-  const left = input.substring(0, periodPosition);
-  const right = input.substr(periodPosition);
-  return (
-    "$" +
-    left +
-    "." +
-    Number(pad(right, numberOfDecimals, "0", "left")).toFixed(numberOfDecimals)
-  );
+  const periodPosition = input.toString().indexOf(".");
+  if (periodPosition < 0) {
+    return "$" + input + "." + pad("", numberOfDecimals, "0", "left");
+  }
+  const left = input.toString().substring(0, periodPosition);
+  const right = input.toString().substr(periodPosition + 1);
+  return "$" + left + "." + pad(right, numberOfDecimals, "0", "left");
 }
