dcd58baa98b64e56ae5544e20fa23324a312f279
C5m7b4 <c5m7b4@gmail.com>
2024-06-11 16:31:16 -0500

    added size to button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -5,9 +5,10 @@ const Btn = styled.button<{
   $color: string;
   $hoverColor: string;
   $hoverText: string;
+  $size: string;
 }>`
-  padding: 10px 20px;
-  font-size: 1.2rem;
+  padding: ${(props) => (props.$size === "small" ? "5px 10px" : "10px 20px")};
+  font-size: ${(props) => (props.$size === "small" ? "1rem" : "1.2rem")};
   background-color: ${(props) => props.$bgColor};
   border-radius: 8px;
   box-shadow: 2px 4px 6px rgba(0, 0, 0, 0.3);
@@ -24,15 +25,17 @@ const Btn = styled.button<{
 `;
 
 type ButtonType = "normal" | "danger" | "warning" | "info";
+type ButtonSize = "small" | "medium" | "large";
 
 interface Props {
   type: ButtonType;
   label: string;
   onClick: () => void;
+  size?: ButtonSize;
 }
 
 const Button = (props: Props) => {
-  const { type, label, onClick } = props;
+  const { type, label, onClick, size = "large" } = props;
 
   let bgColor = "#0d6efd";
   let color = "#000000";
@@ -75,6 +78,7 @@ const Button = (props: Props) => {
       $hoverText={hoverText}
       className="btn btn-primary"
       onClick={onClick}
+      $size={size}
     >
       {label}
     </Btn>
