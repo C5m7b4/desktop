1373633c22bc081f476005ff87587b5a9851d636
C5m7b4 <c5m7b4@gmail.com>
2024-06-18 10:50:53 -0500

    removes duplicate events from being subscribed to

--- src/pubsub/pubsub.ts
+++ src/pubsub/pubsub.ts
@@ -11,14 +11,17 @@ export const SubEvent = {
   list: new Map(),
   on(eventType: eventTypes, eventAction: eventActionType) {
     this.list.has(eventType) || this.list.set(eventType, []);
-    if (this.list.get(eventType)) {
+    const listLength = this.list.get(eventType).length;
+    if (listLength === 0) {
       this.list.get(eventType).push(eventAction);
     }
     return this;
   },
   emit(eventType: eventTypes, args: string) {
+    console.log(`emit triggered`);
     this.list.get(eventType) &&
       this.list.get(eventType).forEach((cb: eventActionType) => {
+        console.log(`emitting event ${eventType}`);
         cb(args);
       });
   },
