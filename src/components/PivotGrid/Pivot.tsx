40bb30c9c7d9b97c23bc89512e539ac877bf9183
C5m7b4 <c5m7b4@gmail.com>
2024-06-24 09:52:01 -0500

    fixes subrows spacing

--- src/components/PivotGrid/Pivot.tsx
+++ src/components/PivotGrid/Pivot.tsx
@@ -20,12 +20,17 @@ const SubRow = styled.div`
   width: 100%;
 `;
 
-export const Td = styled.span<{ $width: number; $align: string }>`
+export const Td = styled.span<{
+  $width: number;
+  $align: string;
+  $bold: boolean;
+}>`
   width: ${(props) => props.$width}px;
   text-align: ${(props) => props.$align};
   overflow: hidden;
   white-space: nowrap;
   text-overflow: ellipsis;
+  font-weight: ${(props) => (props.$bold ? "700" : "400")};
 `;
 
 const SubRowSpacer = styled.span<{ $width: number }>`
@@ -55,6 +60,7 @@ function Pivot<T>(props: Props<T>) {
 
   const tableRef = useRef<HTMLDivElement>(null);
   const tableBodyRef = useRef<HTMLDivElement>(null);
+  const tableOuterBodyRef = useRef<HTMLDivElement>(null);
 
   const { data, rows, setRows, values, setValues, resized } = props;
 
@@ -66,22 +72,25 @@ function Pivot<T>(props: Props<T>) {
   }, [values, rows]);
 
   useEffect(() => {
-    renderTableBody();
+    console.log("useEffect resize occured");
     calculateTableHeight();
+    renderTableBody();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [resized]);
+  }, [resized, data]);
 
   const calculateTableHeight = () => {
-    if (tableRef.current && tableBodyRef.current) {
+    if (tableRef.current && tableBodyRef.current && tableOuterBodyRef.current) {
       const parent = tableRef.current.parentElement;
       const parentbox = parent?.getBoundingClientRect();
       tableRef.current.style.height = `${parentbox?.height}px`;
+      tableRef.current.style.width = `${parentbox?.width}px`;
       const bodyBox = tableBodyRef.current.getBoundingClientRect();
-      if (parentbox) {
-        if (bodyBox.height > parentbox?.height) {
-          tableRef.current.style.overflowY = "scroll";
+      if (parentbox && tableBodyRef.current) {
+        if (bodyBox.height > parentbox.height) {
+          tableOuterBodyRef.current.style.overflowY = "scroll";
+          tableOuterBodyRef.current.style.height = `${parentbox.height - 25}px`;
         } else {
-          tableRef.current.style.overflowY = "";
+          tableBodyRef.current.style.overflowY = "";
         }
       }
     }
@@ -138,6 +147,7 @@ function Pivot<T>(props: Props<T>) {
       if (Array.isArray(parentObject)) {
         return (
           <Td
+            className="pivot-agg-arr"
             $align={"right"}
             $width={calculateWidth() as number}
             key={`tr-td-${i}`}
@@ -152,11 +162,16 @@ function Pivot<T>(props: Props<T>) {
       } else {
         return (
           <Td
+            className="pivot-agg-obj"
             $align={"right"}
             $width={calculateWidth() as number}
             key={`tr-td-${i}`}
           >
-            {applyFormatter(v.fn(parentObject["data"], v.label), v.formatter!)}
+            {applyFormatter(
+              v.fn(parentObject["data"], v.label),
+              v.formatter!,
+              v.decimals!
+            )}
           </Td>
         );
       }
@@ -169,31 +184,24 @@ function Pivot<T>(props: Props<T>) {
   };
 
   const buildSubRows = (subrows, i) => {
-    //const subrows = data[r];
-
     if (!Array.isArray(subrows)) {
       subrows = sortObject(subrows);
       return Object.keys(subrows).map((s, idx) => {
         if (s !== "data") {
           return (
             <div key={`sub-row-${idx}`}>
-              <SubRow>
-                <Td $width={calculateWidth() as number} $align={"left"}>
+              <SubRow className="pivot_subrow">
+                <Td
+                  className="pvito-sub-row-td-for-s"
+                  $bold={false}
+                  $width={calculateWidth() as number}
+                  $align={"left"}
+                >
                   <SubRowSpacer $width={10 + i}>&nbsp;</SubRowSpacer>
 
                   <span>{s}</span>
                 </Td>
-                {values.map((v, i) => {
-                  return (
-                    <Td
-                      $width={calculateWidth() as number}
-                      $align={"right"}
-                      key={`subrow-v-${i}`}
-                    >
-                      {buildAggregates(subrows[s])}
-                    </Td>
-                  );
-                })}
+                {buildAggregates(subrows[s])}
               </SubRow>
               {buildSubRows(subrows[s], i)}
             </div>
@@ -220,34 +228,36 @@ function Pivot<T>(props: Props<T>) {
 
   const renderTableBody = () => {
     return (
-      <div
-        ref={tableBodyRef}
-        className="seudo-table-body"
-        style={{ padding: "4px 6px", position: "relative" }}
-        onContextMenu={handleBodyContext}
-      >
-        {showBodyContextMenu ? (
-          <BodyContextMenu
-            top={points.y}
-            left={points.x}
-            isShowing={showBodyContextMenu}
-            hide={() => setShowBodyContextMenu(false)}
-          />
-        ) : null}
-        {Object.keys(data).map((r, i) => {
-          return (
-            <TableRow
-              key={`table-row=${i}`}
-              rows={rows}
-              i={i}
-              calculateWidth={calculateWidth}
-              r={r}
-              buildSubRows={buildSubRows}
-              buildAggregates={buildAggregates}
-              data={data}
+      <div ref={tableOuterBodyRef}>
+        <div
+          ref={tableBodyRef}
+          className="seudo-table-body"
+          style={{ padding: "4px 6px", position: "relative" }}
+          onContextMenu={handleBodyContext}
+        >
+          {showBodyContextMenu ? (
+            <BodyContextMenu
+              top={points.y}
+              left={points.x}
+              isShowing={showBodyContextMenu}
+              hide={() => setShowBodyContextMenu(false)}
             />
-          );
-        })}
+          ) : null}
+          {Object.keys(data).map((r, i) => {
+            return (
+              <TableRow
+                key={`table-row=${i}`}
+                rows={rows}
+                i={i}
+                calculateWidth={calculateWidth}
+                r={r}
+                buildSubRows={buildSubRows}
+                buildAggregates={buildAggregates}
+                data={data}
+              />
+            );
+          })}
+        </div>
       </div>
     );
   };
